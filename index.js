e67739b08e5ee96908821689a4bb0aeea1b62e6e
Joshua Campbell <joshua.campbell@vgs.io>
2022-03-30 16:23:34 -0700

    Move x5c into key and added required parameters

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const chain = args.slice(1)
 const keystore = jose.JWK.createKeyStore();
 const chainvals = chain.map(file => fs.readFileSync(file, 'ascii'))
 
+for(var i=0;i<chainvals.length;i++){
+    chainvals[i] = chainvals[i].split('\n').slice(1,-2).join('')
+}
+
 keystore
-	.add(key, 'pem')
-	.then(_ => {
-		const jwks = keystore.toJSON();
-		jwks.x5c = chain ? chainvals : undefined;
-		console.log(JSON.stringify(jwks, null, 4));
-	});
+    .add(key, 'pem')
+    .then(_ => {
+        const jwks = keystore.toJSON();
+        jwks.keys[0].x5c = chain ? chainvals : undefined;
+        jwks.keys[0].use = "enc";
+        jwks.keys[0].alg = "RSA-OAEP";
+        jwks.keys[0].expires_on = 2222222222;
+        console.log(JSON.stringify(jwks, null, 4));
+    });
