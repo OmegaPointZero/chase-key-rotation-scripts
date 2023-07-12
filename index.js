a44571aad929fc7453aa52d6208b4bd9afa9bd0d
Joshua Campbell <joshua.campbell@vgs.io>
2023-07-12 12:25:16 -0700

    Updates and automations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jose = require('node-jose');
+const crypto = require('crypto');
 const fs = require('fs');
 
 const chain = process.argv.slice(2);
@@ -14,9 +15,12 @@ keystore
 	.add(key, 'pem')
 	.then(_ => {
 		const jwks = keystore.toJSON();
-		jwks.keys[0].x5c = chain ? chainvals : undefined;
+		jwks.keys[0].x5c = chain ? [chainvals[0]] : undefined;
 		jwks.keys[0].use = "enc";
 		jwks.keys[0].alg = "RSA-OAEP-256";
-		jwks.keys[0].expires_on = 2222222222; // Non-standard -- required by Chase.
+		var sha = crypto.createHash('sha1')
+		sha.update(Buffer.from(chainvals[0], 'base64'));
+		jwks.keys[0].x5t = sha.digest('base64');;
+		jwks.keys[0].expires_on = Date.now() + 60 * 60 * 24 * 1000 * 365;
 		console.log(JSON.stringify(jwks, null, 4));
 	});
